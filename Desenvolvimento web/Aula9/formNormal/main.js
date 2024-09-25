2fb849d34861a8fcfa5427db00b5b447fd1df49e
Fábio Santos <fabiospinheira@gmail.com>
2024-09-24 21:54:50 -0300

    api fetch

---
+++ Desenvolvimento web/Aula9/formNormal/main.js
@@ -0,0 +1,11 @@
+function somar() {
+  const num1 = document.getElementById("num1").value;
+  const num2 = document.getElementById("num2").value;
+  const resultado = document.getElementById("resultado");
+
+  if (num1 === "" || num2 === "") {
+    alert("Devem ser inseridos os números.");
+  } else {
+    resultado.innerHTML = parseFloat(num1) + parseFloat(num2);
+  }
+}
