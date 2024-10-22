334e1638b449c2af8296501cdac2dea9c88b1a6a
Fábio Santos <fabiospinheira@gmail.com>
2024-10-22 20:09:57 -0300

    put and delete

--- Spring boot/src/main/java/com/example/carro/controllers/CarroController.java
+++ Spring boot/src/main/java/com/example/carro/controllers/CarroController.java
@@ -11,9 +11,7 @@ import java.util.Random;
 @RequestMapping("/carros")
 public class CarroController {
 
-
     @GetMapping
-    @ResponseBody
     public List<Carro> buscarTodosCarros() {
 
         List<Carro> carros = new ArrayList<>();
@@ -24,12 +22,45 @@ public class CarroController {
         return carros;
     }
 
-    @PostMapping
-    @ResponseBody
-    public Carro salvarNovoCarro(@RequestBody Carro novoCarro) {
+    @GetMapping("/{id}")
+    public Carro buscarCarro(@PathVariable(name = "id") Long codigo) {
+        return new Carro(1L, "Ford", "Focus", 2010);
+    }
 
+    @DeleteMapping("/{id}")
+    public String deletarCarro(@PathVariable Long id) {
+        return "Carro " + id + " deletado com sucesso!";
+    }
+
+    @PutMapping("/{id}")
+    public void atualizarCarro(@PathVariable Long id, @RequestBody Carro carro) {
+
+        Carro carroBancoDados = this.buscarCarroBancoDados(id);
+
+        if (carroBancoDados != null) {
+            carroBancoDados.setMarca(carro.getMarca());
+            carroBancoDados.setModelo(carro.getModelo());
+            carroBancoDados.setAnoFabricacao(carro.getAnoFabricacao());
+
+            this.salvarBancoDados(carroBancoDados);
+            System.out.println(carro.toString());
+            System.out.println("Atualizado com sucesso: " + id);
+        }
+    }
+
+    @PostMapping
+    public Carro salvarNovoCarro(@RequestBody Carro novoCarro) {
         novoCarro.setId(new Random().nextLong(50));
 
         return novoCarro;
+    }
+
+    private void salvarBancoDados(Carro novoCarro) {
+        // Simulação de salvar no banco de dados
+        System.out.println("Carro salvo com sucesso: " + novoCarro.toString());
+    }
+
+    private Carro buscarCarroBancoDados(Long id) {
+        return new Carro(id, "Ford", "nhe", 2010);
     }
 }
