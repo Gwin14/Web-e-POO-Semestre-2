7f5e3839c9f31318fec758e35d3f2fc298b55cc7
Fábio Santos <fabiospinheira@gmail.com>
2024-10-22 21:46:34 -0300

    db connect

--- Spring boot/src/main/java/com/example/carro/entity/Carro.java
+++ Spring boot/src/main/java/com/example/carro/entity/Carro.java
@@ -1,5 +1,6 @@
 package com.example.carro.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,22 @@ import lombok.Setter;
 @Setter // vai gerar todos os metodos setters dos atributos
 @AllArgsConstructor // vai gerar o construtor com todos os atributos
 @NoArgsConstructor // vai gerar o construtor com nenhum atributo
+
+@Table(name = "db-carro")
+@Entity
 public class Carro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 150)
     private String marca;
+
+    @Column(nullable = false)
     private String modelo;
+
+    @Column(nullable = false, name = "ano_fabricacao")
     private int anoFabricacao;
 
 }
