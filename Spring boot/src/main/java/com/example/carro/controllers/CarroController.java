0e61798aa81d67d5b5679827012cb8d77a8d74e4
Fábio Santos <fabiospinheira@gmail.com>
2024-11-05 16:13:18 -0300

    Update CarroController.java

--- Spring boot/src/main/java/com/example/carro/controllers/CarroController.java
+++ Spring boot/src/main/java/com/example/carro/controllers/CarroController.java
@@ -3,13 +3,12 @@ package com.example.carro.controllers;
 import com.example.carro.entity.Carro;
 import com.example.carro.repository.CarroRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @RestController
 @RequestMapping("/carros")
@@ -19,30 +18,48 @@ public class CarroController {
     private final CarroRepository repository;
 
     @GetMapping
-    public List<Carro> buscarTodosCarros() {
-        return repository.findAll();
+    public ResponseEntity<List<Carro>> buscarTodosCarros() {
+        List<Carro> carros = repository.findAll();
+        return ResponseEntity.ok(carros);
     }
 
     @GetMapping("/{id}")
-    public Optional<Carro> buscarCarro(@PathVariable(name = "id") Long codigo) {
-        return repository.findById(codigo);
+    public ResponseEntity<?> buscarCarro(@PathVariable Long id) {
+        Optional<Carro> carro = repository.findById(id);
+        if (carro.isPresent()) {
+            return ResponseEntity.ok(carro.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Carro com ID " + id + " não encontrado.");
+        }
     }
 
     @PostMapping
-    public Carro salvarNovoCarro(@RequestBody Carro novoCarro) {
+    public ResponseEntity<Carro> salvarNovoCarro(@RequestBody Carro novoCarro) {
         novoCarro.setId(null);
-        return repository.save(novoCarro);
+        Carro carroSalvo = repository.save(novoCarro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(carroSalvo);
     }
 
     @DeleteMapping("/{id}")
-    public void deletarCarro(@PathVariable Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<?> deletarCarro(@PathVariable Long id) {
+        Optional<Carro> carroExistente = repository.findById(id);
+        if (carroExistente.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.ok("Carro com ID " + id + " deletado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Carro com ID " + id + " não encontrado.");
+        }
     }
 
     @PutMapping("/{id}")
-    public void atualizarCarro(@PathVariable Long id, @RequestBody Carro carro) {
-        carro.setId(id);
-        repository.save(carro);
+    public ResponseEntity<?> atualizarCarro(@PathVariable Long id, @RequestBody Carro carro) {
+        Optional<Carro> carroExistente = repository.findById(id);
+        if (carroExistente.isPresent()) {
+            carro.setId(id);
+            Carro carroAtualizado = repository.save(carro);
+            return ResponseEntity.ok(carroAtualizado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Carro com ID " + id + " não encontrado.");
+        }
     }
-
 }
